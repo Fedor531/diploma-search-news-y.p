3a893e3070249cdbb313df50e033e961c5c12110
Fedor <qwertyfredya@gmail.com>
2020-11-14 10:50:28 +0700

    add block search-news and favicon

--- webpack.config.js
+++ webpack.config.js
@@ -68,11 +68,10 @@ module.exports = {
       canPrint: true
     }),
     new HtmlWebpackPlugin({
-      // Означает, что:
-      inject: false, // стили НЕ нужно прописывать внутри тегов
+      inject: false,
       hash: true,
-      template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
-      filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+      template: './src/index.html',
+      filename: 'index.html'
     }),
     new WebpackMd5Hash(),
     new webpack.DefinePlugin({
