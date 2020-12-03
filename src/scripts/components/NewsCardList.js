e813c047067eb487d3959b67945b837c0eb91f88
Fedor <qwertyfredya@gmail.com>
2020-12-04 00:02:27 +0700

    refactor NewsCards

--- src/scripts/components/NewsCardList.js
+++ src/scripts/components/NewsCardList.js
@@ -2,6 +2,8 @@ export default class NewsCardList {
   constructor($container, buildCardItem) {
     this.$container = $container;
     this.buildCardItem = buildCardItem;
+    this._cards = null;
+    this._cardNumbers = 3;
   }
 
   _addCard(placeElement) {
@@ -9,9 +11,18 @@ export default class NewsCardList {
   }
 
   renderCards(cards) {
-    cards.forEach(item => {
+    this.$container.innerHTML = ''
+    if (cards) {
+      this._cards = cards
+      this._cardNumbers = 3
+    }
+    this._cards.some((item, index) => {
       const newsCard = this.buildCardItem(item)
       this._addCard(newsCard.createCardHtml())
+      if ((index + 1) === this._cardNumbers) {
+        this._cardNumbers += 3
+        return true
+      }
     });
   }
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,44 +3,65 @@ import NewsApi from './modules/NewsApi'
 import NewsCardList from './components/NewsCardList'
 import NewsCard from './components/NewsCard'
 
+const $preloader = document.querySelector('.preloader')
+const $searchResultsContainer = document.querySelector('.search-results')
+const $newsCardsContainer = document.querySelector('.cards')
+const $nothingFoundContainer = document.querySelector('.nothing-found')
+
+const $searchForm = document.querySelector('.search-news__form')
+const $searchInput = document.querySelector('.search-news__input')
+const $showMoreCardsButton = document.querySelector('.search-results__show-more-button')
 
 const newsApiConfig = {
   apiKey: 'a3d7340b4fb145859582c09cb7e3de16',
   pageSize: 100
 }
 
-const $newsCardsContainer = document.querySelector('.cards')
-
 const buildCardItem = (cardData) => new NewsCard(cardData)
 
 const newsCardList = new NewsCardList($newsCardsContainer, buildCardItem)
 const api = new NewsApi(newsApiConfig)
 
-api.getNews('Федя')
-  .then((res) => {
-    console.log(res)
-    renderLoading(true)
-    const cards = res.articles
-    newsCardList.renderCards(cards)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-  .finally(() => {
-    renderLoading(false)
-  })
-
-const preloader = document.querySelector('.preloader')
-const searchResultsContainer = document.querySelector('.search-results')
-
-
-function renderLoading(isLoading) {
+/* function renderLoading(isLoading) {
   if (isLoading) {
-    preloader.setAttribute('style', 'display:block')
-    searchResultsContainer.setAttribute('style', 'display:none')
+    $preloader.setAttribute('style', 'display:block')
+    $searchResultsContainer.setAttribute('style', 'display:none')
   } else {
-    preloader.setAttribute('style', 'display:none')
-    searchResultsContainer.setAttribute('style', 'display:block')
+    $preloader.setAttribute('style', 'display:none')
+    $searchResultsContainer.setAttribute('style', 'display:block')
   }
+} */
+
+function seacrhNews(event) {
+  event.preventDefault()
+  $preloader.setAttribute('style', 'display:block')
+  $nothingFoundContainer.setAttribute('style', 'display:none')
+  $searchResultsContainer.setAttribute('style', 'display:none')
+  api.getNews($searchInput.value.trim())
+    .then((res) => {
+      console.log(res)
+      const cards = res.articles
+      if (cards.length === 0) {
+        $nothingFoundContainer.setAttribute('style', 'display:block')
+       } else {
+        newsCardList.renderCards(cards)
+        $searchResultsContainer.setAttribute('style', 'display:block')
+       }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    .finally(() => {
+      $preloader.setAttribute('style', 'display:none')
+    })
 }
+
+function showMoreCards() {
+  newsCardList.renderCards()
+}
+
+
+$showMoreCardsButton.addEventListener('click', showMoreCards)
+$searchForm.addEventListener('submit', seacrhNews)
+
 
