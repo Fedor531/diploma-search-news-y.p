490e4e3e6967b24953e697cec469a83ca41f289a
Fedor <>
2021-12-28 11:15:11 +0200

    refactor: card style

--- src/scripts/components/NewsCard.js
+++ src/scripts/components/NewsCard.js
@@ -8,7 +8,7 @@ export default class NewsCard {
         this._cardImage = this._cardData.urlToImage ? this._cardData.urlToImage : 'https://i.ytimg.com/vi/3e0FsU1N6OQ/hqdefault_live.jpg';
 
         const markUp = `
-        <div class="card">
+        <a class="card" href="${ this._cardData.url }" target="_blank">
           <img class="card__image" src="${ this._cardImage }" alt="Картинка новости">
           <div class="card__content">
             <p class="card__date">${ this.createTimeFormat(this._cardData.publishedAt) }</p>
@@ -16,7 +16,7 @@ export default class NewsCard {
             <div class="card__subtitle">${ this._cardData.description }</div>
             <p class="card__source">${ this._cardData.source.name }</p>
           </div>
-        </div>`;
+        </a>`;
 
         const element = document.createElement('div');
         element.insertAdjacentHTML('afterbegin', markUp.trim());
--- src/scripts/modules/GithubApi.js
+++ src/scripts/modules/GithubApi.js
@@ -3,7 +3,7 @@ export default class GithubApi {
         if (!res.ok) {
             return Promise.reject(`Ошибка: ${ res.status }`);
         }
-        
+
         return res.json();
     }
 
